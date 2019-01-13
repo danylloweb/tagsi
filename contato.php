b3a215ef5a377044138d43ec0a7396d67e79d962
Danyllo7622 <danyllophp@gmail.com>
2019-01-13 20:05:05 -0200

    first commit

---
+++ contato.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Desenvolvido Por Danyllo-php">
+    <link rel="shortcut icon" href="img/favicon.png">
+    <meta name="description" content=" A Tag Epóxi é uma empresa com a mais alta experiência na apresentação de soluções e aplicação de RAD" />
+    <meta name="keywords" content="tag-epoxi,pisos epoxi,revestimentos epoxi,epoxi,epoxi recife" />
+    <meta name="author" content="Danyllo Ferreira Santos da Silva" />
+    <meta name="robots" content="index, follow" />
+    <meta property="og:title" content="inicial">
+    <meta property="og:description" content=" A Tag Epóxi é uma empresa com a mais alta experiência na apresentação de soluções e aplicação de RAD">
+    <meta property="og:url" content="http://tagsi.com.br/">
+    <meta property="og:locale" content="pt_BR">
+    <meta property="og:site_name" content="Tag Epóxi">
+    <meta property="og:type" content="website">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <title>
+        Tag - Epóxi
+    </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/theme.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/flexslider.css"/>
+    <link href="assets/bxslider/jquery.bxslider.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.carousel.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.theme.css">
+
+    <link href="css/superfish.css" rel="stylesheet" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'> -->
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" type="text/css" href="css/component.css">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+
+    <link rel="stylesheet" type="text/css" href="css/parallax-slider/parallax-slider.css" />
+    <script type="text/javascript" src="js/parallax-slider/modernizr.custom.28468.js">
+    </script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js">
+    </script>
+    <script src="js/respond.min.js">
+    </script>
+    <![endif]-->
+</head>
+
+<body>
+<!--header start-->
+<header class="head-section">
+    <div class="navbar navbar-default navbar-static-top container">
+        <div class="navbar-header">
+            <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="http://tagsi.com.br/"><img style="position: relative; top:-.2em" width="220px;"
+                                                          src="img/tag-logo.png"></a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="http://tagsi.com.br/">Empresa</i>
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="epoxi.html">Tipos de Revestimento Epoxi</i>
+                    </a>
+                </li>
+                <li>
+                    <a href="contato.php">Contato</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
+<!--header end-->
+<!--breadcrumbs start-->
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-4">
+                <h1>
+                    Contato
+                </h1>
+            </div>
+            <div class="col-lg-8 col-sm-8">
+                <ol class="breadcrumb pull-right">
+                    <li>
+                        <a href="http://tagsi.com.br/">
+                            Empresa
+                        </a>
+                    </li>
+                    <li class="active">
+                        Contato
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+<!--breadcrumbs end-->
+
+
+
+<!--container start-->
+<div class="container">
+
+
+    <div class="row">
+        <div class="col-lg-5 col-sm-5 address">
+            <section class="contact-infos">
+                <h4 class="title custom-font text-black">
+                    Email
+                </h4>
+                <p>
+                    <i class="fa fa-envelope"></i> contato@tagsi.com.br
+                </p>
+            </section>
+            <section class="contact-infos">
+                <h4 class="title custom-font text-black">
+                    Skype
+                </h4>
+                <p>
+                   <i class="fa fa-skype"></i> tag.solucoes.industriais
+                </p>
+            </section>
+            <section class="contact-infos">
+                <h4>
+                    Telefone
+                </h4>
+                <p>
+                    <i class="fa fa-phone">
+                    </i>
+                    (81)3077-8313
+                </p>
+
+                <p>
+                    <i class="fa fa-phone">
+                    </i>
+                    81 98260 1614
+                </p>
+                <p>
+                    <i class="fa fa-phone">
+                    </i>
+                     81 98135 9444
+                </p>
+
+            </section>
+        </div>
+        <div class="col-lg-7 col-sm-7 address">
+            <h4>
+                Teremos o prazer em lhe atender
+            </h4>
+            <div class="contact-form">
+                <form role="form" method="POST">
+                    <div class="form-group">
+                        <label for="name">
+                            Nome
+                        </label>
+                        <input type="text" placeholder="" id="name" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">
+                            Email
+                        </label>
+                        <input type="text" placeholder="" id="email" name="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">
+                           Assunto
+                        </label>
+                        <input type="text" id="subject" name"subject" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">
+                            Mensagem
+                        </label>
+                <textarea placeholder="" rows="5" class="form-control" id="message" name="message">
+                </textarea>
+                    </div>
+                    <button class="btn btn-info" type="button" onclick="envia()">
+                        Enviar
+                    </button>
+                </form>
+
+            </div>
+            <script type="text/javascript">
+                function envia() {//envia formulario via ajax para email
+                    var nome = $('#name').val();
+                    var email = $('#email').val();
+                    var assunto = $('#subject').val();
+                    var mensagem = $('#message').val();
+
+                    $.ajax({
+                        type: "POST",
+                        crossDomain: true,
+                        url: "email.php",
+                        data: {
+                            'nome': nome,
+                            'email': email,
+                            'assunto': assunto,
+                            'message': mensagem
+                        },
+                        success: function (data) {
+                            alert("Mensagem enviada com sucesso");
+                            location.href = 'contato.php'
+                        },
+                        error: function (data) {
+                            alert("erro ao enviar");
+                            location.href = 'contato.php'
+                        }
+                    });
+
+    }
+
+
+            </script>
+        </div>
+    </div>
+
+</div>
+<!--container end-->
+
+<!--google map start-->
+<div class="contact-map">
+    <div id="map-canvas" style="width: 100%; height: 400px">
+    </div>
+</div>
+<!--google map end-->
+
+<!--footer start-->
+<footer class="footer">
+    <div class="container">
+
+    </div>
+</footer>
+<!-- footer end -->
+<!--small footer start -->
+<footer class="footer-small">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="copyright">
+                    <p style="text-align: center">&copy; Copyright - Todos os direitos reservados - Tag Epóxi - Desenvolvido por |
+                        Danyllo Ferreira
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--small footer end-->
+<!-- js placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js">
+</script>
+<script src="js/bootstrap.min.js">
+</script>
+<script type="text/javascript" src="js/hover-dropdown.js">
+</script>
+<script type="text/javascript" src="assets/bxslider/jquery.bxslider.js">
+</script>
+
+
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&AMP;sensor=false">
+</script>
+
+<!--common script for all pages-->
+<script src="js/common-scripts.js">
+</script>
+<script src="js/wow.min.js">
+</script>
+<script>
+    wow = new WOW(
+        {
+            boxClass:     'wow',      // default
+            animateClass: 'animated', // default
+            offset:       0          // default
+        }
+    )
+    wow.init();
+</script>
+
+
+<script>
+    $(document).ready(function() {
+            //Set the carousel options
+            $('#quote-carousel').carousel({
+                    pause: true,
+                    interval: 4000,
+                }
+            );
+        }
+    );
+
+    //google map
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(-8.0399574,-34.8836728);
+        var mapOptions = {
+            zoom: 15,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: 'Contact'
+            }
+        );
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+</script>
+
+<!-- WiredMinds eMetrics tracking with Enterprise Edition V5.4 START -->
+<script type='text/javascript' src='https://count.carrierzone.com/app/count_server/count.js'></script>
+<script type='text/javascript'><!--
+wm_custnum='a6e7b5a2221d9f34';
+wm_page_name='contato.php';
+wm_group_name='/services/webpages/t/a/tagsi.com.br/public';
+wm_campaign_key='campaign_id';
+wm_track_alt='';
+wiredminds.count();
+// -->
+</script>
+<!-- WiredMinds eMetrics tracking with Enterprise Edition V5.4 END -->
+</body>
+</html>
